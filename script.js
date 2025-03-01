832fa2fd943d87123595a75ecb4f4ff513c6c942
weihuabing31 <weihuabing31@163.com>
2025-03-01 16:46:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const noTexts = [
   "不许选这个！ ",
   "我会很伤心…",
   "不行:(",
+  "娄芯语你这家伙",
 ];
 
 // No 按钮点击事件
@@ -43,8 +44,8 @@ noButton.addEventListener("click", function () {
   mainImage.style.transform = `translateY(-${moveUp}px)`;
   questionText.style.transform = `translateY(-${moveUp}px)`;
 
-  // No 文案变化（前 5 次变化）
-  if (clickCount <= 5) {
+  // No 文案变化（前 6 次变化）
+  if (clickCount <= 6) {
     noButton.innerText = noTexts[clickCount - 1];
   }
 
